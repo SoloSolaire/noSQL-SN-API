2803860a62feb2a8b0807f67a323fbc1b366c73a
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-08-14 17:36:49 -0700

    added routes

---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
+
+router.route("/").get(getAllThoughts).post(createThought);
+
+router.route("/:id").get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route('/:id/reactions').post(addReaction);
+
+router.route("/:id/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
